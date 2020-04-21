61a04e0e2b6ab4c3e9399c53ff00576c49621b59
lt-studio <tranglek1@hotmail.com>
2020-04-21 10:23:23 +0200

    restructure folder, remove screenshots

---
+++ 1_code/01_3_Intro to R_Vectors.R
@@ -0,0 +1,114 @@
+#2. VECTORS
+
+#2.1 Create a vector####
+
+#Vectors are one-dimension arrays that can hold numeric data, character data, or logical data.
+#Create a vector with the combine function c(), place the vector elements separated by a comma.
+numeric_vector <- c(1, 2, 3)
+character_vector <- c("a", "b", "c")
+boolean_vector <- c(TRUE, FALSE, TRUE)
+#E.g, make an analysis for a gamble games in Las Vegas
+#Create a data vector for poker game: 
+#On Monday you won $140
+#Tuesday you lost $50
+#Wednesday you won $20
+#Thursday you lost $120
+#Friday you won $240
+poker_vector <- c(140, -50, 20, -120, 240) #negative number for losing money
+#Create a data vector for roulette game:
+#On Monday you lost $24
+#Tuesday you lost $50
+#Wednesday you won $100
+#Thursday you lost $350
+#Friday you won $10
+roulette_vector <- c(-24, 50, 100, -350, 10)
+
+#2.2 Naming a vector####
+
+#It is important to have a clear view on the data that you are using. 
+#Understanding what each element refers to is essential
+#Give a name to the elements of a vector with the names() function
+some_vector <- c("John Doe", "poker player")
+names(some_vector) <- c("Name", "Profession")
+#See how it appears with names
+some_vector
+#Continue with the Las Vegas exercise, assign the days as name
+names(poker_vector) <- c("Monday","Tuesday","Wednesday","Thursday","Friday")
+names(roulette_vector) <- c("Monday","Tuesday","Wednesday","Thursday","Friday")
+#Print out to see the results
+poker_vector
+roulette_vector
+#To avoid typing names of the days everytime, create a variable for that
+days_vector <- c("Monday","Tuesday","Wednesday","Thursday","Friday")
+#Assign the name again
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+#2.3 Calculating vectors####
+
+#You want to find out the following type of information:
+#How much has been your overall profit or loss per day of the week?
+#Have you lost money over the week in total?
+#Are you winning/losing money on poker or on roulette?
+#To get the answers, you have to do arithmetic calculations on vectors.
+#If you sum two vectors, it takes the element-wise sum. 
+#For example, the following three statements are completely equivalent:
+c(1,2,3) + c(4,5,6)
+c(1+4,2+5,3+6)
+c(5,7,9)
+#You can also do the calculations with variables that represent vectors
+A_vector <- c(1,2,3)
+B_vector <-  c(4,5,6)
+total_vector <- A_vector + B_vector
+total_vector #sum 2 vectors
+#Find out the total sum of profit/lost on each day
+total_daily <- poker_vector + roulette_vector
+total_daily
+#Use sum function to calculate the sum of all elements of a vector
+total_poker <- sum(poker_vector)
+total_roulette <- sum(roulette_vector)
+#Total winning overall
+total_week <- total_poker + total_roulette
+total_week #16$ winning in total
+#Compare total winnings by ">"
+total_poker > total_roulette #TRUE (you play poker better than roulette)
+
+#2.4 Vector selection####
+
+#Select specific element of a vector with square brackets []
+#Select Wednesday which has the index 3 in the vector and assign a new variable
+poker_wednesday <- poker_vector[3]
+poker_wednesday #20$ winning on wednesday in poker game
+#Select multiple elements from a vector by combine funcion in square brackets
+poker_midweek <- poker_vector[c(2,3,4)]
+#Select contnuous elements from a vector by ":", e.g [2:5]
+roulette_selection_vector <- roulette_vector[2:5]
+#Since the vectors are already assigned with names, we can also select names instead of index number
+poker_vector["Monday"] #winning 140$ on Monday
+#Select poker results for Monday, Tuesday and Wednesday
+poker_start <- poker_vector[c("Monday","Tuesday","Wednesday")]
+#Calculate the average of the selected elements with mean function
+mean(poker_start) #winning 36.57$
+
+#2.5 Selection by comparison####
+
+#You can also subset the selection by using comparison. The logical comparison operators are:
+#< for less than
+#> for greater than
+#<= for less than or equal to
+#>= for greater than or equal to
+#== for equal to each other
+#!= not equal to each other
+#Comparison operators can be also used on vectors
+c(4, 5, 6) > 5 #FALSE FALSE  TRUE
+#Find out which days of winning
+selection_vector <- poker_vector > 0
+#This tests for every element of the vector if the condition stated by the comparison operator is TRUE or FALSE.
+selection_vector
+#Select the winning days
+poker_winning_days <- poker_vector[selection_vector]
+poker_winning_days
+#Similarly, which days did you make money on roulette?
+selection_vector <- roulette_vector > 0
+roulette_winning_days <- roulette_vector[selection_vector]
+roulette_winning_days
---
+++ 1_code/01_5_Intro to R_Data frames.R
@@ -0,0 +1,88 @@
+#5. DATA FRAMES
+
+#5.1 What is a data frame?####
+
+#You will often work with data sets that contain different data types instead of only one. 
+#A data frame has the variables of a data set as columns and the observations as rows.
+#Load a built-in example data frame in R called "mtcars"
+mtcars
+#When you work with large data sets and data frames, it's better to develop a clear understanding of its structure and main elements. 
+#Therefore, it is often useful to show only a small part of the entire data set. 
+head(mtcars) #head() shows the first observations of a data frame
+#Similarly, tail() function shows the last observations of a data frame
+tail(mtcars)
+
+#5.2 Structure of a data frame####
+
+#To get a rapid overview of your data is the structure function: str()
+#The total number of observations
+#The total number of variables
+#A full list of the variable names
+#The data type of each variable
+#The first observations
+#Applying the str() function will often be the first thing that you do when receiving a new data set or data frame. 
+#It is a great way to get more insight in your data set before diving into the real analysis.
+str(mtcars) 
+
+#5.3 Create a data frame####
+
+#Construct a data frame that describes the main characteristics of eight planets in our solar system
+name <- c("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+#Add the type of planet (Terrestrial or Gas Giant)
+type <- c("Terrestrial planet", "Terrestrial planet", "Terrestrial planet", "Terrestrial planet", "Gas giant", "Gas giant", "Gas giant", "Gas giant")
+#Add the planet's diameter relative to the diameter of the Earth
+diameter <- c(0.382, 0.949, 1, 0.532, 11.209, 9.449, 4.007, 3.883)
+#Add the planet's rotation across the sun relative to that of the Earth
+rotation <- c(58.64, -243.02, 1, 1.03, 0.41, 0.43, -0.72, 0.67)
+#If the planet has rings or not (TRUE or FALSE).
+rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
+#Now create a data frame from those vectors
+planets_df <- data.frame(name, type, diameter, rotation, rings)
+#The vectors become different columns of the data frame
+#Because every column has the same length, the vectors should also have the same length
+planets_df 
+#Check the structure of the data frame
+str(planets_df)
+
+#5.4 Selection of data frame elements####
+
+#Similar to vectors and matrices, select elements from a data frame with square brackets [ ]
+#By using a comma, you can indicate what to select from the rows and the columns respectively.
+#E.g, print out diameter of Mercury (row 1, column 3)
+planets_df[1,3] #0.382
+#Print out data for Mars (entire fourth row)
+planets_df[4,] #(1.5 Data frame 5)
+#Instead of using numerics, it's also possible to use the variable names to select columns of a data frame.
+planets_df[1:3,2] 
+#Or, it is often easier to just make use of the variable name:
+planets_df[1:3, "type"]
+#If you want to select all elements of the variable "diameter":
+planets_df[,3]
+#Or
+planets_df[,"diameter"]
+#But the shorter way is using "$" symbol
+planets_df$diameter
+#Select the rings variable from "planets_df"
+rings_vector <- planets_df$rings
+#See which planet has a ring
+rings_vector #FALSE FALSE FALSE FALSE  TRUE  TRUE  TRUE  TRUE
+#This means that the first four observations do not have a ring, but the other four do. 
+#However, you do not get a nice overview of the names of these planets, their diameter, etc. 
+#Select all columns for planets with rings
+planets_df[rings_vector,] #to select all columns, leave the columns part inside the [ ] empty
+#Or create a subset with a condition from the data frame
+subset(planets_df, subset = rings) 
+
+#5.5 Sorting####
+
+#In data analysis you can sort data according to a certain variable in the data set.
+#order() is a function that gives the ranked position of each element when it is applied on a variable
+a <- c(100, 10, 1000)
+order(a)
+#This means we can use the output of order(a) to reshuffle a:
+a[order(a)]
+#Rearrange the data frame "planets_df" such that it starts with the smallest planet and ends with the largest one.
+#Sorting on "diameter" column
+positions <- order(planets_df$diameter)
+#Use positions to sort "planets_df"
+planets_df[positions,]
