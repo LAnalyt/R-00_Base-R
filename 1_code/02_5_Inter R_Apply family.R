750327933a36b8787467ff90c4fec030a053e740
ltrangng <tranglek1@hotmail.com>
2020-06-11 10:54:43 +0200

    Lappy

--- 1_code/02_5_Inter R_Apply family.R
+++ 1_code/02_5_Inter R_Apply family.R
@@ -1,3 +1,73 @@
 # 5. THE APPLY FAMILY
 
 # 5.1 The Lapply ####
+
+# When using R, you will encounter vectors and lists containing all sorts of information. We could use for loop to iterate over all kinds of data structures. But there's even an easier way with Lapply.
+# Reconstruct again the previous list example
+nyc <- list(pop = 8405837, 
+            boroughs = c("Manhatten", "Bronx", "Brooklyn", "Queens",                          "Staten Islands"), 
+            capital = FALSE)
+# Suppose you want to find out the class of each element in this list
+for(info in nyc) {
+  print(class(info))
+}
+# The for loop is, however, a long code just to find out the class of the elements in the list. Instead we could use lappy()
+lapply(nyc, class)
+
+# Another example with city names
+cities <- c("New York", "Paris", "London", "Tokyo", "Rio de Janiero", "Cape Town")
+# Suppose we want to build a vector of the same length, containing the number of characters of each city name.
+num_chars <- c() # create an empty vector
+for(i in 1:length(cities)) { # using loop index
+  num_chars[i] <- nchar(cities[i]) #use nchar() function
+}
+num_chars # we got the result, but it's a lot of code.
+# Refactoring this code using lapply() is easy
+lapply(cities, nchar) # result a list containing the number of characters of the corresponding cities in the input vector.
+# Lapply always returns a list, irrespective of the input data structure. By using lapply, the code looks much more intuitive and readable.
+
+# Lapply can also be used not just with default functions but customized functions.
+# Assume we have a list of oil prices per gallon
+oil_prices <- list(2.37, 2.49, 2.18, 2.22, 2.47, 2.32)
+# Write a function that triples every element of oil_prices
+triple <- function(x) {
+  3 * x
+}
+# Apply this triple function on each element using lapply()
+lapply(oil_prices, triple)
+# Try to make this triple function more generic
+multiply <- function(x, factor) { # use an additional argument
+  x * factor
+}  # we can now choose which factor to multiply the input.
+# Use multiply inside the lapply function
+lapply(oil_prices, multiply, factor = 4)
+
+# To put it generally, lapply takes a vector or a list x, and applies a specified function to its members. If the function requires additional arguments, it could be passed inside the lapply() function
+lapply(x, FUNC, ...) # syntax lapply
+# The output of lapply() is a list, where each element is the result of appply FUNC on the corresponding element of X.
+
+# Create a vector or ramous mathematicians/statisticians and the year they were born
+pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
+# Split names from birth year using strplit() fucntion
+split_math <- strsplit(pioneers, split = ":") # split the strings on the : sign.
+# Convert to lowercase strings using tolower() function
+split_low <- lapply(split_math, tolower) 
+# Inspect the contents of split_low
+str(split_low) # names are in converted to lowercase now.
+# Now write new functions that allows to select the names and the years separately
+select_first <- function(x){
+  x[1]
+}
+select_second <- function(x){
+  x[2]
+}
+# Apply the new functions
+names <- lapply(split_low, select_first)
+years <- lapply(split_low, select_second)
+# Or generic select function
+select_el <- function(x, index) {
+  x[index]
+}
+# Use lapply() with additional arguments
+names <- lapply(split_low, select_el, index = 1)
+years <- lapply(split_low, select_el, index = 2)
