50aaa0c2157c4337512263e00a8a3d350a397751
Trang Ng <59824573+ltrangng@users.noreply.github.com>
2020-11-02 17:54:03 +0100

    packages

--- 1_code/2_4_Inter R_Packages.R
+++ 1_code/2_4_Inter R_Packages.R
@@ -1,4 +1,4 @@
-# 4. R PACKAGES
+# 4. R PACKAGES 
 
 # R packages are bundles of code, data, documentation, and tests that are easy to share with others. E.g, the mean(), list() and sample() functions are part of the Base R package, which contains the basic functionality to use R.
 
@@ -9,25 +9,25 @@ install.packages("ggvis") # this function goes to CRAN, download and install the
 
 # 4.2 Load packages ####
 
-# When R loads a package, it actually attaches it to the search list
+# When R loads a package, it actually attaches it to the search list.
 search() # list of packages and environments that R looks through to find the variable or function you want to use.
 # GlobalEnv: workspace, where the user-defined R objects are stored.
-# Try to access a function in the installed ggvis packages
+# Try to access a function in the installed ggvis packages:
 ggvis(mtcars, ~wt, ~hp) # return an error, because ggvis is not yet in the search list.
-# Load ggvis manually
+# Load ggvis manually:
 library("ggvis")
-# Check once more
+# Check once more:
 search() # ggvis is now a part of the list.
-# Run the ggvis function again
+# Run the ggvis function again:
 ggvis(mtcars, ~wt, ~hp) # print out a graph.
 
-# Another way to load packages is to use require() function for loading packages that are not yet installed
+# Another way to load packages is to use require() function for loading packages that are not yet installed.
 require("data.table") # return a warning if attaching package fails.
-# Check if the package is already attached
+# Check if the package is already attached:
 (result <- require("data.table")) # TRUE/FALSE
 # This is a good alternative to avoid errors when attaching dynamically packages inside functions.
 
-# Load ggplot2 package
+# Load ggplot2 package:
 require("ggplot2")
-# Build a plot of two variables of the mtcars data frame using quick plot function from the package ggplot2
+# Build a plot of two variables of the mtcars data frame using quick plot function from the package ggplot2:
 qplot(mtcars$wt, mtcars$hp)
