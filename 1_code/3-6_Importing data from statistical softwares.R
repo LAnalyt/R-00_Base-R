307ae109a1e46c5ae22f6654481a3dcc8969768c
Trang Ng <59824573+ltrangng@users.noreply.github.com>
2021-02-07 22:46:06 +0100

    import data from statistical softwares

---
+++ 1_code/3-6_Importing data from statistical softwares.R
@@ -0,0 +1,10 @@
+# 6. IMPORT DATA FROM STATISTICAL SOFTWARES
+
+# 6.1 Statistical software packages ####
+# Next to R, there are also other commonly used statistical software packages. Each of them has their own file format. 
+# SAS (.sas7bdat, sas7bcat): most widely used in Business Analytics, Biostatistics and Medical Sciences.
+# STATA (.dta): typical tool for economists.
+# SPSS (.sav, .por): often used in Social Sciences.
+# No matter which package the data comes from, it can be imported to R, most commonly with haven or foreign package.
+
+
