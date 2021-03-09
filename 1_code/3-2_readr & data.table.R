482c8f9eb91b3faff61ecd5d4d804a4e614d6e62
Trang Ng <59824573+ltrangng@users.noreply.github.com>
2021-03-09 22:36:44 +0100

    html readr & data.table

--- 1_code/3-2_readr & data.table.R
+++ 1_code/3-2_readr & data.table.R
@@ -3,16 +3,15 @@
 # In addition to base R, there are dedicated packages to easily and efficiently import flat file data. 
 
 # 2.1 readr ####
-# readr of the tidyverse package is fast and easy to use with consistent naming, while utils is more verbose and multiple slower.
+# readr of the tidyverse package is fast and easy to use with consistent naming, while utils is more verbose and multiple times slower.
 library(readr)
 # Similar function like read.csv in readr is read_csv:
-read_csv("states.csv")
-# The result is pretty much the same. The only difference is read_csv ouputs a tibble, which is a supercharged version of data frame. The printout conveniently shows the column classes.
+read_csv("states.csv") # returns a tibble.
 # readr also provides read_tsv (tab separated value) which is a similar function to read.delim.
 read_tsv("states.txt")
 # In both cases there is no need to specify stringsAsFactors explicitly, because readr doesn't import strings automatically as factors. Just like in utils package, both read_csv and read_tsv are wrappers around the mother import function read_delim().
-# Import the potatoes csv data which gives information on the impact of storage period and cooking on potatoes' flavor. 
-read_csv("potatoes.csv")
+# Import the "potatoes.csv" data 
+read_csv("potatoes.csv") # gives information on the impact of storage period and cooking on potatoes' flavor.
 # Use read_tsv for txt data instead:
 read_tsv("potatoes.txt")
 # This txt file does not contain columns names in the first row. This can be specify manually.
@@ -51,7 +50,7 @@ read_delim("potatoes.txt", delim = "\t",
 # col_integer(): the column should be interpreted as an integer.
 # col_factor(levels, ordered = FALSE): the column should be interpreted as a factor with levels.
 
-# Import hotdogs.txt without col_types
+# Import hotdogs.txt without col_types:
 hotdogs <- read_tsv("hotdogs.txt", 
                     col_names = c("type", "calories", "sodium"))
 summary(hotdogs)
