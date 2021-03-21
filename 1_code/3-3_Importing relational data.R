37357eb055f8a4d9af5b222724dd42ed80fdad64
Trang Ng <59824573+ltrangng@users.noreply.github.com>
2021-03-21 17:01:19 +0100

    rename

--- 1_code/3-3_Importing relational data.R
+++ 1_code/3-3_Importing relational data.R
@@ -1,6 +1,6 @@
-# 4. IMPORTING RELATIONAL DATA
+# 3. IMPORTING RELATIONAL DATA
 
-# 4.1 Relational databases ####
+# 3.1 Relational databases ####
 # Many companies store their information in relational databases. The information is stored in separate tables whose identifier can be connected to each other through a database management system (DBMS). # Open-source DBMS: MySQL, postgreSQL, SQLite...
 # Proprietary DBMS: Oracle Database, Microsoft SQL Server
 # Practically all DBMS use SQL language for querying and maintaining database. Depending on the type of database you want to connect to, you will have to use different packages.
@@ -10,7 +10,7 @@ library(DBI)
 # PostgresSQL: RPostgresSQL package
 # Oracale Database: ROracle package
 
-# 4.2 Connecting to a database ####
+# 3.2 Connecting to a database ####
 # dbConnect() creates a connection between your R session and a SQL database. Create a connection to the remote MySQL database:
 con <- dbConnect(RMySQL::MySQL(),    # construct  SQL driver
                  dbname = "tweater", # specify database's name
@@ -21,7 +21,7 @@ con <- dbConnect(RMySQL::MySQL(),    # construct  SQL driver
 # The result of dbConnect() is a DBI connection object. This object has to be passed to any function for interacting with the database.
 con 
 
-# 4.3 Import table data ####
+# 3.3 Import table data ####
 # After connecting successfully to a database, we want to see what is in there. The first step is listing all the tables in the database.
 tables <- dbListTables(con)
 # The database contains data on a more tasty version of Twitter, namely Tweater. Users can post tweats with short recipes for delicious snacks. People can comment on these tweats. 
@@ -33,7 +33,7 @@ dbReadTable(con, "users") # specify table in second argument.
 lapply(tables, dbReadTable, conn = con)
 # The tweats table contains a column user_id. The ids in the column refer to the users that have posted the tweat. Similarly, the comments contain both a user_id and a tweat_id column. It specifies which user posted a comment on which tweat.
 
-# 4.4 SQL queries from inside R ####
+# 3.4 SQL queries from inside R ####
 # dbReadTable() imports the entire table, which is not optimal with large database. Selective importing only imports the elements that you actually need inside R via SQL language.
 # SQL queries: retrieve data based on specific criteria. 
 dbGetQuery(con, "SELECT name FROM users WHERE id = 1")
